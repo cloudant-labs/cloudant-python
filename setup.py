56fc32a31d0fc389e001eded1d1e6f2a19b7226c
Max Thayer <garbados@gmail.com>
2013-06-02 02:08:32 -0400

    fixed setup.py dependencies

--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@
 
 from setuptools import setup
 
+with open('requirements.txt', 'r') as f:
+  dependencies = [line.strip() for line in f.readlines()]
+
 setup(name='Divan',
       version='0.0.3',
       description='Effortless CouchDB ODM',
@@ -9,5 +12,6 @@ setup(name='Divan',
       author_email='garbados@gmail.com',
       url='https://github.com/garbados/divan',
       packages=['divan'],
+      install_requires=dependencies,
       test_suite="test"
       )
