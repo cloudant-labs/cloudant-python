3108249974a9bd5aad4041aa9f2767c27c9d5f7a
Max Thayer <garbados@gmail.com>
2013-10-26 21:47:41 -0400

    updated docs and docs generator

--- docs/__main__.py
+++ docs/__main__.py
@@ -1,5 +1,5 @@
 import inspect
-import divan
+import lib as divan
 import jinja2
 import re
 import os
@@ -9,59 +9,65 @@ maindir = os.path.normpath(os.path.join(dirname, '..'))
 docs = {}
 
 def is_module(item):
-  return inspect.ismodule(item)
+    return inspect.ismodule(item)
 
 def is_private(method_name):
-  return bool(method_name[0] == "_")
+    return bool(method_name[0] == "_")
 
 def is_function(method):
-  return hasattr(method, '__call__')
+    return hasattr(method, '__call__')
 
 def get_first_indent(string):
-  return re.match(r'(\s{2,})\w', string).groups()[0]
+    match = re.match(r'(\s{2,})\w', string)
+    if match: 
+        return match.groups()[0]
+    else: 
+        return match
 
 def get_docs(method):
-  try:
-    arg_spec = get_args(method)
-  except TypeError:
-    arg_spec = get_args(method.__init__)
-  docs = {
-    'args': ', '.join(arg_spec.args[1:]),
-    'kwargs': arg_spec.keywords
-  }
-  if method.__doc__:
-    first_indent = get_first_indent(method.__doc__)
-    docs['docs'] = method.__doc__.replace(first_indent, '\n')
-  return docs
+    try:
+        arg_spec = get_args(method)
+    except TypeError:
+        arg_spec = get_args(method.__init__)
+    docs = {
+        'args': ', '.join(arg_spec.args[1:]),
+        'kwargs': arg_spec.keywords
+    }
+    if method.__doc__:
+        first_indent = get_first_indent(method.__doc__)
+        if first_indent:
+            docs['docs'] = method.__doc__.replace(first_indent, '\n')
+        else:
+            docs['docs'] = '\n' + method.__doc__ + '\n'
+    return docs
 
 def get_args(method):
-  return inspect.getargspec(method)
+    return inspect.getargspec(method)
 
 for item_name in dir(divan):
-  item = getattr(divan, item_name)
-  if not (is_module(item) or is_private(item_name)):
-    docs[item_name] = {}
-    docs[item_name]['_main'] = get_docs(item)
-    for method_name in dir(item):
-      method = getattr(item, method_name)
-      if is_function(method) and not is_private(method_name):
-        if not method.__doc__:
-          print "WARNING: %s.%s has no documentation!" % (item_name, method_name)
-        docs[item_name][method_name] = get_docs(method)
+    item = getattr(divan, item_name)
+    if not (is_module(item) or is_private(item_name)):
+        docs[item_name] = {}
+        docs[item_name]['_main'] = get_docs(item)
+        for method_name in dir(item):
+            method = getattr(item, method_name)
+            if is_function(method) and not is_private(method_name):
+                if not method.__doc__:
+                    print "WARNING: %s.%s has no documentation!" % (item_name, method_name)
+                docs[item_name][method_name] = get_docs(method)
 
 with open(os.path.join(dirname, 'readme_template.md'), 'r') as f:
-  template = jinja2.Template(f.read())
+    template = jinja2.Template(f.read())
 
 with open(os.path.join(maindir,'readme.md'), 'w') as f:
-  print os.path.join(maindir,'readme.md')
-  f.write(template.render(**{
-    'docs': docs,
-    'order': [
-      'Connection', 
-      'Database',
-      'Document',
-      'View',
-      'Attachment',
-      'Error'
-    ]
-  }))
+    print os.path.join(maindir,'readme.md')
+    f.write(template.render(**{
+        'docs': docs,
+        'order': [
+            'Connection', 
+            'Database',
+            'Document',
+            'View',
+            'Attachment'
+        ]
+    }))
