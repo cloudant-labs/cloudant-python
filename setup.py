6aa3d520b39bb39ba395b16e381bae3345def7bc
Max Thayer <garbados@gmail.com>
2013-11-04 11:13:10 -0500

    version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='Cloudant',
-      version='0.2.4',
+      version='1.0.0',
       description='Asynchronous Cloudant / CouchDB Interface',
       author='Max Thayer',
       author_email='garbados@gmail.com',
