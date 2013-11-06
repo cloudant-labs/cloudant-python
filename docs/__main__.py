06523164d25dea42a4c32c2444f0766c968bec44
Max Thayer <garbados@gmail.com>
2013-11-05 19:26:21 -0500

    gitignore is tricky tricky

--- docs/__main__.py
+++ docs/__main__.py
@@ -14,7 +14,7 @@ dirname, filename = os.path.split(os.path.abspath(__file__))
 maindir = os.path.normpath(os.path.join(dirname, '..'))
 
 README = os.path.normpath(os.path.join(dirname, '..', 'readme.md'))
-OUTPUT = os.path.join(dirname, 'dist', 'index.html')
+OUTPUT = os.path.join(dirname, 'site', 'index.html')
 TEMPLATE = os.path.join(dirname, 'templates', 'index.html')
 
 util.generate_docs(ORDER, TEMPLATE, OUTPUT, README)
