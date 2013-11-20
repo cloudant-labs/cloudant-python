e59e8ea2525e473c1cd7904bd65a8d895da21d0f
Max Thayer <garbados@gmail.com>
2013-11-20 11:23:44 -0500

    remove long description so folks can install

--- setup.py
+++ setup.py
@@ -8,9 +8,8 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(name='cloudant',
-      version='0.5.0',
+      version='0.5.1',
       description='Asynchronous Cloudant / CouchDB Interface',
-      long_description=read('README.rst'),
       author='Max Thayer',
       author_email='garbados@gmail.com',
       url='https://github.com/cloudant-labs/cloudant',
