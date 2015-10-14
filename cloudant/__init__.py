25232e03457d8ccd7d11b7b2379bfd4cbf4f0cee
Adam Cox <gadamc@cloudant.com>
2015-10-14 14:27:04 -0700

    Updates warning message

--- cloudant/__init__.py
+++ cloudant/__init__.py
@@ -7,13 +7,18 @@ from .attachment import Attachment
 from .index import Index
 
 import warnings
-message = """You are using version 0.5.10 of this library,
-             which is now deprecated. It will be replaced 
-             with version 2.0.0 in early 2016. This will 
-             introduce breaking changes. Please upgrade as
-             soon as possible. Find out more at 
-             https://github.com/cloudant/python-cloudant
-          """
+message = """
+*********************************************
+`cloudant` Deprecation Warning
+
+You are using version 0.5.10 of this library,
+which is now deprecated. It will be replaced 
+with version 2.0.0 in early 2016. This will 
+introduce breaking changes. Please upgrade as
+soon as possible. Find out more at 
+https://github.com/cloudant/python-cloudant
+*********************************************
+"""
 
 #we don't use DeprecationWarning because that message is ignored by default
 warnings.warn(message) 
